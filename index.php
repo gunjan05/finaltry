f3a028659084be31b068b98df351ad7a0836ae46
Gunjan <you@example.com>
2019-07-18 21:52:19 +0530

    Second commit

--- index.php
+++ index.php
@@ -42,6 +42,13 @@ body {
   <a href="#about">About</a>
 </div>
 
+<div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="https://www.instagram.com/namo_soaps/?hl=en">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+</div>
+
 <div style="padding-left:16px">
   <h2>Top Navigation Example</h2>
   <p>Some content..</p>
